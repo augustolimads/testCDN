6d4647dd8a45d88b752e0dea5beb7e72ed4c806a
Augusto Lima <augustolimads@gmail.com>
2022-03-15 19:44:28 -0300

    add promise

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 console.log('hello cdn');
 
-const dataTest = {
-  valueA: 2,
-  valueB: ['a', 'b', 'c'],
-};
+const dataTest = { valueA: 2, valueB: ['a', 'b', 'c'] };
 
 const value = 1;
+
+const testPromise = new Promise((resolve, reject) => {
+  resolve({ dataTest, value });
+});
